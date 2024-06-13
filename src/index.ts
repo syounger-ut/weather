938733c8a0f8b837a1b22ee46842cfcef1e6b820
samyounger <sam.younger@gmail.com>
2024-06-13 23:28:48 +0100

    Request module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import 'dotenv/config';
 import https from 'https';
-import { ClientRequest, IncomingMessage } from "node:http";
+import { request } from './request';
 
 const options: https.RequestOptions = {
   method: 'GET',
@@ -8,23 +7,5 @@ const options: https.RequestOptions = {
   path: `/swd/rest/observations?token=${process.env.TEMPEST_TOKEN}&device_id=${process.env.TEMPEST_DEVICE_ID}`,
 };
 
-const req: ClientRequest = https.request(options, (res: IncomingMessage) => {
-  let data: any[] = [];
-  const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
-  console.log('Status Code:', res.statusCode);
-  console.log('Date in Response header:', headerDate);
-
-  res.on('data', chunk => {
-    data.push(chunk);
-  });
-
-  res.on('end', () => {
-    console.log('Response ended: ');
-    const payload = JSON.parse(Buffer.concat(data).toString());
-    console.log('payload: ', payload);
-  });
-}).on('error', err => {
-  console.log('Error: ', err.message);
-});
-
-req.end();
+const req = request(options);
+req((payload: JSON) => console.log(payload));
---
+++ src/request.ts
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import https from 'https';
+import { ClientRequest, IncomingMessage } from "node:http";
+
+const dispatch = (requestOptions: https.RequestOptions, callback: (payload: any) => any): ClientRequest => (
+  https.request(requestOptions, (res: IncomingMessage) => {
+    let data: any[] = [];
+    const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+    console.log('Status Code:', res.statusCode);
+    console.log('Date in Response header:', headerDate);
+
+    res.on('data', chunk => {
+      data.push(chunk);
+    });
+
+    res.on('end', () => {
+      console.log('Response ended: ');
+      const payload = JSON.parse(Buffer.concat(data).toString());
+
+      callback(payload);
+    });
+  }).on('error', err => {
+    console.log('Error: ', err.message);
+  })
+);
+
+type RequestCallback = (resolve: (payload: JSON) => unknown) => void;
+export const request = (requestOptions: https.RequestOptions): RequestCallback => (
+  (resolve: (payload: JSON) => unknown): void => {
+    const req = dispatch(requestOptions, resolve);
+
+    req.end();
+  }
+);
+
