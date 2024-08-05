6715e068d9bf0422684aad71cb4a148dcc6543ff
samyounger <sam.younger@gmail.com>
2024-08-05 23:11:48 +0100

    Observation model

---
+++ src/models/observation.ts
@@ -0,0 +1,26 @@
+class Observation {
+    public constructor(
+        public dateTime: number,
+        public windLull: number,
+        public windAvg: number,
+        public windGust: number,
+        public windDirection: number,
+        public windSampleInterval: number,
+        public pressure: number,
+        public airTemperature: number,
+        public relativeHumidity: number,
+        public illuminance: number,
+        public uv: number,
+        public solarRadiation: number,
+        public rainAccumulation: number,
+        public precipitationType: number,
+        public avgStrikeDistance: number,
+        public strikeCount: number,
+        public battery: number,
+        public reportInterval: number,
+        public localDayRainAccumulation: number,
+        public ncRainAccumulation: number,
+        public localDayNCRainAccumulation: number,
+        public precipitationAnalysis: number,
+    ) {}
+}
