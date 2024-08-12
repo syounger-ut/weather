b5f13fc99ea0e9a8c7e9948fa3980b2e8cc19ad6
Sam Younger <syounger@usertesting.com>
2024-08-12 23:40:41 +0100

    POC fetching and listing buckets with SSO auth credentials

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import 'dotenv/config';
 import { request } from './utils/request';
 import { routes } from './utils/routes';
 import { parsePayload } from './utils/observationDevice';
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
+import { fromSSO } from '@aws-sdk/credential-provider-sso';
+
 
 const observationsRoute = routes['/observations'];
 
@@ -11,9 +14,31 @@ const fetchObservation = async () => {
   return await req(handleResponse);
 };
 
+const fetchBuckets = async () => {
+  const credentials = await fromSSO({ profile: process.env.AWS_PROFILE })();
+
+  const client = new S3Client({
+    region: 'eu-west-2',
+    credentials,
+  });
+
+  const helloS3 = async () => {
+    const command = new ListBucketsCommand({});
+
+    const { Buckets } = await client.send(command);
+    console.log("Buckets: ");
+    console.log(Buckets?.map((bucket) => bucket.Name).join("\n"));
+    return Buckets;
+  };
+
+  console.log('helloS3: ', await helloS3());
+};
+
 const handler = async (event: any) => {
   const response = await fetchObservation();
   console.log('response: ', response);
+
+  await fetchBuckets();
 
   return {
     statusCode: 200,
