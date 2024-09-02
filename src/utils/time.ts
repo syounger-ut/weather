5b7923479e2d438048d380a0e94fd6f0487f76ca
Sam Younger <syounger@usertesting.com>
2024-09-02 10:43:00 +0100

    Time util documentation

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -12,12 +12,22 @@ export const formatDateToString = (date: Date): string => {
   return `${year}/${month}/${day}/${hour}/`;
 };
 
+/*
+ * Get start and end seconds of a day
+ * @param {Date} date
+ * @returns {object}
+ */
 export const dateStartEndSeconds = (date: Date): { start: number, end: number } => {
   const startTime = Math.floor(date.setHours(0, 0, 0, 0) / 1000);
   const endTime = Math.floor(date.setHours(23, 59, 59, 999) / 1000)
   return { start: startTime, end: endTime };
 };
 
+/*
+ * Convert unix timestamp to Date object
+ * @param {number} unix
+ * @returns {Date}
+ */
 export const unixToDateTime = (unix: number): Date => (
   new Date(unix * 1000)
 );
