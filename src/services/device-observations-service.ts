c7561ccae31d2cac78865576d0604e7e6538cfd6
Sam Younger <syounger@usertesting.com>
2024-09-02 10:39:44 +0100

    Make logs more readable with shorter payloads

--- src/services/device-observations-service.ts
+++ src/services/device-observations-service.ts
@@ -8,9 +8,11 @@ export class DeviceObservationsService {
 
   public async fetchAndInsertReading(): Promise<{ insertCount: number, reading: Device }> {
     const reading = await this.observationsService.readObservation();
-    console.debug('reading: ', reading);
+    console.debug('readings: ', { ...reading, observations: reading.observations.length });
+    console.debug('inserting readings: ', reading.observations.map((obs) => obs.dateTime.toString()));
     const readingInserts = reading.observations.map(obs => this.observationsService.insertReading(obs, obs.dateTime.toString()));
     const insertResult = await Promise.allSettled(readingInserts);
+    console.debug('inserted readings: ', insertResult.length);
 
     return {
       insertCount: insertResult.length,
