76e5ba4510f14837a874329e8e2cec774d7ef028
samyounger <sam.younger@gmail.com>
2024-09-05 23:31:16 +0100

    fetchObservations scripts paths updated

--- services/fetchObservations/bin/02-build-layer.sh
+++ services/fetchObservations/bin/02-build-layer.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -eo pipefail
-mkdir -p lib/nodejs
-rm -rf node_modules lib/nodejs/node_modules
-npm install --omit=dev
-cp -r node_modules lib/nodejs/
+
+LIB_NODE_MODULES="$(dirname "$0")"/../lib/nodejs/node_modules
+NODE_MODULES="$(dirname "$0")"/../node_modules
+LIB_NODEJS="$(dirname "$0")"/../lib/nodejs
+
+mkdir -p "$LIB_NODEJS"
+rm -rf "$NODE_MODULES" "$LIB_NODE_MODULES"
+npm install --omit=dev --workspace=fetchObservations
+cp -r "$NODE_MODULES" "$LIB_NODEJS"
