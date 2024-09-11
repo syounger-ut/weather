061fe9c697f05769805f4dd22ca87f94675faa7e
samyounger <sam.younger@gmail.com>
2024-09-11 22:36:39 +0100

    JSON extension to observation files

--- packages/storeObservations/src/services/device-observations-service.ts
+++ packages/storeObservations/src/services/device-observations-service.ts
@@ -10,7 +10,11 @@ export class DeviceObservationsService {
     const reading = await this.observationsService.readObservation();
     console.debug('readings: ', { ...reading, observations: reading.observations.length });
     console.debug('inserting readings: ', reading.observations.map((obs) => obs.dateTime.toString()));
-    const readingInserts = reading.observations.map(obs => this.observationsService.insertReading(obs, obs.dateTime.toString()));
+    const readingInserts = reading.observations.map(obs => {
+      console.log('obs: ', obs);
+      const fileName = obs.dateTime.toString() + '.json';
+      this.observationsService.insertReading(obs, fileName);
+    });
     const insertResult = await Promise.allSettled(readingInserts);
     console.debug('inserted readings: ', insertResult.length);
 
