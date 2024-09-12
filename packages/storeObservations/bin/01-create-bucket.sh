1c8361c6580dd3daa5f11e5feeb9bdae88a9df10
samyounger <sam.younger@gmail.com>
2024-09-12 23:10:05 +0100

    StoreObservations script fix

--- packages/storeObservations/bin/01-create-bucket.sh
+++ packages/storeObservations/bin/01-create-bucket.sh
@@ -3,5 +3,5 @@
 set -eo pipefail
 BUCKET_ID=$(dd if=/dev/random bs=8 count=1 2>/dev/null | od -An -tx1 | tr -d ' \t\n')
 BUCKET_NAME=lambda-artifacts-$BUCKET_ID
-echo "$BUCKET_NAME" > "$(dirname "$0")"/../bucket-name.txt
+echo "$BUCKET_NAME" > bucket-name.txt
 aws s3 mb s3://"$BUCKET_NAME"
