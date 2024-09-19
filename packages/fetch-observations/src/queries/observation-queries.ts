972f3b19139222927bbd69fd23a8425bd1f66763
samyounger <sam.younger@gmail.com>
2024-09-19 22:50:42 +0100

    Observation-queries specs

--- packages/fetch-observations/src/queries/observation-queries.ts
+++ packages/fetch-observations/src/queries/observation-queries.ts
@@ -3,10 +3,9 @@ export class ObservationQueries {
   private static TABLE_NAME = 'observations';
 
   public static getObservationsByDateRange(columns: string[], { year, month, day, hourMin = '00', hourMax = '23' }: DateProps): string {
-    return `
-      SELECT ${columns.join(',')} FROM ${this.TABLE_NAME}
-      WHERE year='${year}' AND month='${month}' AND day='${day}' AND hour>='${hourMin}' AND hour<='${hourMax}'
-      ORDER BY windDirection DESC LIMIT 100;
-    `;
+    return `\n
+      SELECT ${columns.join(',')} FROM ${this.TABLE_NAME}\n
+      WHERE year='${year}' AND month='${month}' AND day='${day}' AND hour>='${hourMin}' AND hour<='${hourMax}'\n
+      ORDER BY windDirection DESC LIMIT 100;`;
   }
 }
